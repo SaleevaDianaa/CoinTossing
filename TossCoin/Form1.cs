9043df0ddfa2a902a49e6420244cfb550a62369e
SaleevaDianaa <sdianaas06@gmail.com>
2024-11-06 15:33:45 +0200

    FirstCommit

---
+++ TossCoin/Form1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TossCoin.Properties;
+
+namespace TossCoin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
